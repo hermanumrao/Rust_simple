6cb222fc278e58fcb1266a3adf604cf6b8891603
hermanumrao <hermanumrao@gmail.com>
2024-05-05 23:19:18 +0530

    included hashing

--- 10_hashing/hashmap.rs
+++ 10_hashing/hashmap.rs
@@ -1,11 +1,33 @@
-fn main() {
-    let number=4;
-    let state = match number {
-        1 => "baby",  // normal case
-        2 |3|5|7|11 => println!("This is a prime"); // 2 or 3 or 5 or....
-        13..=19 -> println!("a teen");  // any number between 13 to 19
-        _ => println("your are none of the above")
-    };
-    println!("name={}",state);
-}
+use std::collections::HashMap;
 
+// fn main() {
+//     let mut state = HashMap::new(); // Corrected the call to new() method
+//     state.insert("k", "Kerala");
+//     state.insert("h", "Haryana");
+//
+//     println!("{:?}", state);
+//     println!("size is {}", state.len());
+// }
+
+
+fn main() {
+    let mut state=HashMap::new();
+    state.insert("k","kerla");
+    state.insert("h","herman");
+
+    match state.get (&"kerla") {
+        Some(expr) => {
+            println!("value for key is {}",expr);
+        } 
+        None => {
+            println!("nothing found");
+        }
+    }
+
+    if state.contains_key("k"){
+        println!("gotcha");
+    }
+    else {
+        println!("not found");
+    }
+}
--- 10_hashing/hashset.rs
+++ 10_hashing/hashset.rs
@@ -1,33 +1,22 @@
 use std::collections::HashSet;
 
-// fn main() {
-//     let mut name = HashSet::new();
-//     name.insert("pes");
-//     name.insert("univ");
-//     name.insert("benga");
-//     println!("size= {}",name.len());
-//
-//     for name_i in name.iter(){
-//         println!("{}",name_i);
-//     }
-//     match name.get(&"pes") {
-//         Some(expr) => {println!("found {}",expr); }
-//         None => {println!("niot found");}
-//     }
-// }
-
-
 fn main() {
     let mut name = HashSet::new();
     name.insert("pes");
     name.insert("univ");
     name.insert("benga");
     println!("size= {}",name.len());
+    println!("{:?}", name);
 
     for name_i in name.iter(){
         println!("{}",name_i);
     }
+    match name.get(&"pes") {
+        Some(expr) => {println!("found {}",expr); } // using get 
+        None => {println!("niot found");}
+    }
     if name.contains("pes")  {println!("found "); }
     else {println!("not found");}
-    
 }
+
+
